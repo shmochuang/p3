2f383671e334d05e614d2756ef18411ed7834e02
Shmo Chuang <elisajchuang@gmail.com>
2014-07-17 12:18:12 -0400

    check if input is numeric

--- app/views/randusr.blade.php
+++ app/views/randusr.blade.php
@@ -19,34 +19,40 @@
 
 	<div class="generated users_out">
 	
-		<!-- create users -->
-		<?php $faker = Faker\Factory::create(); ?>
-			
-			<!-- loop thorugh users -->
-			@for($i=0; $i < $_POST['num_u']; $i++)
-			
-				<!-- echo name -->
-				{{ "<li>"."Name: ".$faker->name."</li>"; }}
+		@if(is_numeric($_POST["num_u"]))
+		
+			<!-- create users -->
+			<?php $faker = Faker\Factory::create(); ?>
 				
+				<!-- loop thorugh users -->
+				@for($i=0; $i < $_POST['num_u']; $i++)
 				
-				<!-- if address == on, echo address -->
-				@if ($_POST['address'] == 'on') 
-				{{ "<li>"."Address: ".$faker->address."</li>"; }}
-				@endif
-				
-				
-				<!-- if text == on, echo text -->
-				@if ($_POST['text'] == 'on')
-				{{ "<li>"."Text: ".$faker->text."</li>"; }}
-				@endif
-				
-				<!-- only echo breaks if more users to come -->
-				@if ($i < $_POST['num_u'] - 1)
-				{{ "<br><br>"; }}
-				@endif
-				
-				
-			@endfor
+					<!-- echo name -->
+					{{ "<li>"."Name: ".$faker->name."</li>"; }}
+	
+					
+					<!-- if address == on, echo address -->
+					@if ($_POST['address'] == 'on') 
+					{{ "<li>"."Address: ".$faker->address."</li>"; }}
+					@endif
+					
+					
+					<!-- if text == on, echo text -->
+					@if ($_POST['text'] == 'on')
+					{{ "<li>"."Text: ".$faker->text."</li>"; }}
+					@endif
+					
+					<!-- only echo breaks if more users to come -->
+					@if ($i < $_POST['num_u'] - 1)
+					{{ "<br><br>"; }}
+					@endif
+					
+					
+				@endfor
+		@else
+			{{ "Please input a number."."<br>" }}
+			{{ "<a href='/randusr' alt='random user form page'>Return to form</a>" }} 
+		@endif
 		
 	</div>
 

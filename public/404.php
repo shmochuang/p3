adb946a04f5ecb57723c20dd6dd881fa178a74e7
Shmo Chuang <elisajchuang@gmail.com>
2014-07-16 22:08:24 -0400

    add error page

--- public/404.php
+++ public/404.php
@@ -8,7 +8,7 @@
 <body>
 	<h1>404 Not Found</h1>
 	<p>The content you are looking for is... </p>
-	<img src="<?php echo URL::asset('images/lostmei.jpg'); ?>" alt="lost or under laundry 404 image.">
+	<img src="images/lostmei.jpg" alt="lost or under laundry 404 image.">
 
 
 	<p>Please return to the <a href="javascript:history.go(-1)">previous page</a>.</p>
